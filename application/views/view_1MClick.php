85af7be32adf149452b1a376af0410aeca8ede86
musabtuts <musabtuts@gmail.com>
2014-12-11 12:32:52 +0000

    controller, models, views olusturuldu ve db ayarlari yapildi.

---
+++ application/views/view_1MClick.php
@@ -0,0 +1,257 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,600,900|Playfair+Display:400,900|Montserrat:400,700|Raleway:300,900|Lato:300,900|Nunito:300,700|Open+Sans:300italic,400italic,400,600,700,800&subset=latin,latin-ext" rel="stylesheet" type="text/css">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+        <meta name="apple-mobile-web-app-capable" content="yes">
+        <meta name="description" content="" />
+        <meta charset=utf-8 />
+        <script src="assets/code_javascript/jquery.min.js"></script>
+        
+        <link rel="stylesheet" href="assets/code_css/css.css">
+        <script src="assets/code_javascript/main.js"></script>
+        <title>1MClick</title>
+    </head>
+
+    <body class="mode-1">
+        <button class="status"></button>
+
+        <div class="top-field">
+
+            <header class="header">
+                <p class="header-title"><a href="">1Mclick</a></p>
+                <p class="header-description">Sponsorlardan 50TL ve üzeri indirim kuponu kazanmak için hemen bi' tıklatın.</p>
+            </header>
+
+            <div class="counter outer">
+                <ul class="counter inner">
+                    <li id="digit-7" class="is-n-0">
+                        <ul class="digit outer">
+                            <li class="number inner n-0"><span>0</span>
+                            </li>
+                            <li class="number inner n-1"><span>1</span>
+                            </li>
+                            <li class="number inner n-2"><span>2</span>
+                            </li>
+                            <li class="number inner n-3"><span>3</span>
+                            </li>
+                            <li class="number inner n-4"><span>4</span>
+                            </li>
+                            <li class="number inner n-5"><span>5</span>
+                            </li>
+                            <li class="number inner n-6"><span>6</span>
+                            </li>
+                            <li class="number inner n-7"><span>7</span>
+                            </li>
+                            <li class="number inner n-8"><span>8</span>
+                            </li>
+                            <li class="number inner n-9"><span>9</span>
+                            </li>
+                        </ul>
+                    </li>
+                    <li id="digit-6" class="is-n-0">
+                        <ul class="digit outer">
+                            <li class="number inner n-0"><span>0</span>
+                            </li>
+                            <li class="number inner n-1"><span>1</span>
+                            </li>
+                            <li class="number inner n-2"><span>2</span>
+                            </li>
+                            <li class="number inner n-3"><span>3</span>
+                            </li>
+                            <li class="number inner n-4"><span>4</span>
+                            </li>
+                            <li class="number inner n-5"><span>5</span>
+                            </li>
+                            <li class="number inner n-6"><span>6</span>
+                            </li>
+                            <li class="number inner n-7"><span>7</span>
+                            </li>
+                            <li class="number inner n-8"><span>8</span>
+                            </li>
+                            <li class="number inner n-9"><span>9</span>
+                            </li>
+                        </ul>
+                    </li>
+                    <li id="digit-5" class="is-n-0">
+                        <ul class="digit outer">
+                            <li class="number inner n-0"><span>0</span>
+                            </li>
+                            <li class="number inner n-1"><span>1</span>
+                            </li>
+                            <li class="number inner n-2"><span>2</span>
+                            </li>
+                            <li class="number inner n-3"><span>3</span>
+                            </li>
+                            <li class="number inner n-4"><span>4</span>
+                            </li>
+                            <li class="number inner n-5"><span>5</span>
+                            </li>
+                            <li class="number inner n-6"><span>6</span>
+                            </li>
+                            <li class="number inner n-7"><span>7</span>
+                            </li>
+                            <li class="number inner n-8"><span>8</span>
+                            </li>
+                            <li class="number inner n-9"><span>9</span>
+                            </li>
+                        </ul>
+                    </li>
+                    <li id="digit-4" class="is-n-0">
+                        <ul class="digit outer">
+                            <li class="number inner n-0"><span>0</span>
+                            </li>
+                            <li class="number inner n-1"><span>1</span>
+                            </li>
+                            <li class="number inner n-2"><span>2</span>
+                            </li>
+                            <li class="number inner n-3"><span>3</span>
+                            </li>
+                            <li class="number inner n-4"><span>4</span>
+                            </li>
+                            <li class="number inner n-5"><span>5</span>
+                            </li>
+                            <li class="number inner n-6"><span>6</span>
+                            </li>
+                            <li class="number inner n-7"><span>7</span>
+                            </li>
+                            <li class="number inner n-8"><span>8</span>
+                            </li>
+                            <li class="number inner n-9"><span>9</span>
+                            </li>
+                        </ul>
+                    </li>
+                    <li id="digit-3" class="is-n-0">
+                        <ul class="digit outer">
+                            <li class="number inner n-0"><span>0</span>
+                            </li>
+                            <li class="number inner n-1"><span>1</span>
+                            </li>
+                            <li class="number inner n-2"><span>2</span>
+                            </li>
+                            <li class="number inner n-3"><span>3</span>
+                            </li>
+                            <li class="number inner n-4"><span>4</span>
+                            </li>
+                            <li class="number inner n-5"><span>5</span>
+                            </li>
+                            <li class="number inner n-6"><span>6</span>
+                            </li>
+                            <li class="number inner n-7"><span>7</span>
+                            </li>
+                            <li class="number inner n-8"><span>8</span>
+                            </li>
+                            <li class="number inner n-9"><span>9</span>
+                            </li>
+                        </ul>
+                    </li>
+                    <li id="digit-2" class="is-n-0">
+                        <ul class="digit outer">
+                            <li class="number inner n-0"><span>0</span>
+                            </li>
+                            <li class="number inner n-1"><span>1</span>
+                            </li>
+                            <li class="number inner n-2"><span>2</span>
+                            </li>
+                            <li class="number inner n-3"><span>3</span>
+                            </li>
+                            <li class="number inner n-4"><span>4</span>
+                            </li>
+                            <li class="number inner n-5"><span>5</span>
+                            </li>
+                            <li class="number inner n-6"><span>6</span>
+                            </li>
+                            <li class="number inner n-7"><span>7</span>
+                            </li>
+                            <li class="number inner n-8"><span>8</span>
+                            </li>
+                            <li class="number inner n-9"><span>9</span>
+                            </li>
+                        </ul>
+                    </li>
+                    <li id="digit-1" class="is-n-0">
+                        <ul class="digit outer">
+                            <li class="number inner n-0"><span>0</span>
+                            </li>
+                            <li class="number inner n-1"><span>1</span>
+                            </li>
+                            <li class="number inner n-2"><span>2</span>
+                            </li>
+                            <li class="number inner n-3"><span>3</span>
+                            </li>
+                            <li class="number inner n-4"><span>4</span>
+                            </li>
+                            <li class="number inner n-5"><span>5</span>
+                            </li>
+                            <li class="number inner n-6"><span>6</span>
+                            </li>
+                            <li class="number inner n-7"><span>7</span>
+                            </li>
+                            <li class="number inner n-8"><span>8</span>
+                            </li>
+                            <li class="number inner n-9"><span>9</span>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+                <div class="click-point">
+                    <div id="click" class="click">
+                        <ul>
+                            <li class="slogan-title"><span>Tıkla - Kazan</span>
+                            </li>
+                            <li class="click-title"><span>Tıkla</span>
+                            </li>
+                            <li class="send-title"><span>Çekiliyor...</span>
+                            </li>
+                            <li class="win-title"><span>Kazandın</span>
+                            </li>
+                            <li class="lose-title"><span>Olmadı</span>
+                            </li>
+                        </ul>
+                    </div>
+                    <ul class="result-notify selection-on">
+                        <li class="slogan-description">
+                            <p>1 Milyon tık dolmadan hemen sizde deneyin</p>
+                        </li>
+                        <li class="win-description">
+                            <p>Tebrikler! <span class="winner-count">12345</span>. kişi olarak "<a href="#" class="coupon-owner">250TL TEKNOSA</a>" indirim kuponu kazandınız.Aşağıdaki kodu kopyalayın veya not alın, kullanmak istediğiniz zaman için saklayın ve <a href='javascript:void(0);' onclick='$("#down-content").removeClass().addClass("tab-2");$("body").addClass("button-more-is-active");'>kullanım koşullarını</a> okuyun.</p>
+                            <ul id="coupon-codes" title="Kodu kopyalayın">
+                                <li>Kupon Adı : <span id="coupon-name">250TL-TEKNOSA</span></li>
+                                <li>Kupon Kodu : <span id="coupon-code">ABCD-EFGH-1234-5678</span></li>
+                            </ul>
+                        </li>
+                        <li class="lose-description">
+                            <p>Üzgünüz, herhangi bir kupon kazanamadınız.1 saat sonra tekrar deneyin...</p>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="down-field">
+            <div class="inner">
+                <button class="more with-shadow"></button>
+                <div id="down-content" class="tab-1">
+                    <ul class="tab-buttons">
+                        <li class="tab-title"><span>Sponsorlar</span>
+                        </li>
+                        <li class="tab-title"><span>Kullanım</span>
+                        </li>
+                        <li class="tab-title"><span>Hakkımızda</span>
+                        </li>
+                    </ul>
+                    <ul class="tab-containers">
+                        <li class="tab-content">Sponsorlar</li>
+                        <li class="tab-content">Kullanım</li>
+                        <li class="tab-content">Hakkımızda</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+    </body>
+
+</html>
